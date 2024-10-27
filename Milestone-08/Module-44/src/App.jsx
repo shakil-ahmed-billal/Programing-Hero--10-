470b5e8d395480920d2235e969cc9f7fe4b6b7bd
shakil-ahmed-billal <itzxhakil@gmail.com>
2024-10-27 23:56:50 +0600

    module 44 prectice start or prectice topic tailwind CSS and dasiy UI and react ui components

---
+++ Milestone-08/Module-44/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Banner from './components/Banner'
+import Navber from './components/Navber'
+
+function App() {
+  
+  return (
+    <>
+      <Navber></Navber>
+      <Banner></Banner>
+    </>
+  )
+}
+
+export default App
---
+++ Milestone-08/Module-44/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+   require('daisyui'),
+  ],
+}
+
