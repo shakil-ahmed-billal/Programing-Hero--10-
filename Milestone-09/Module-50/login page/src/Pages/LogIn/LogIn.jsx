d17ed313e6f14b6e2457966fce157e620ceb631b
shakil-ahmed-billal <itzxhakil@gmail.com>
2024-11-10 23:50:17 +0600

    module 49 prectice all complete

--- Milestone-09/Module-50/login page/src/Pages/LogIn/LogIn.jsx
+++ Milestone-09/Module-50/login page/src/Pages/LogIn/LogIn.jsx
@@ -1,7 +1,86 @@
+import {sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from './../../authentication/firebase.init';
+import { useRef, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LogIn = () => {
+
+  const [user , setUsers] = useState(false)
+  const [error , setError] = useState('')
+  const emailRef = useRef()
+
+  const handleLogin = (event) =>{
+    event.preventDefault()
+
+    const email = event.target.email.value
+    const password = event.target.password.value
+    console.log(email , password);
+
+    // error and user message clear
+    setError('')
+    setUsers(false)
+
+    // log in users section
+    signInWithEmailAndPassword(auth , email , password)
+    .then(res=>{
+      console.log(res.user);
+
+      if(!res.user.emailVerified){
+        setError('please verify your email address')
+      }else{
+        setUsers(true)
+      }
+    })
+    .catch(error=>{
+      setError(error.message)
+    })
+  }
+  console.log();
+  const handlePassword = ()=>{
+    sendPasswordResetEmail(auth , emailRef.current.value)
+    .then(() =>{
+      alert('Password reset email send')
+    }).catch(error=>{
+      setError(error.message)
+    })
+  }
   return (
-    <div>LogIn</div>
+
+    <div className="hero bg-base-200 h-[calc(100vh-338px)]">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <div className="">{user?<h1 className="text-5xl text-green-500  font-bold"> SuccessFully Log In Users !</h1>: <h1 className="text-5xl font-bold">Login now!</h1>}</div>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+            quasi. In deleniti eaque aut repudiandae et a id nisi.
+          </p>
+        </div>
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <form onSubmit={handleLogin} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input ref={emailRef} type="email" name="email" placeholder="email" className="input input-bordered" required />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+              <label className="label">
+                <a onClick={handlePassword} href="#" className="label-text-alt link link-hover">Forgot password?</a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Login</button>
+              {error&& <p className="text-center text-red-500">{error}</p>}
+            </div>
+          </form>
+          <p className="text-center pb-2">New to this website Please <Link className="btn ml-2" to={'/signup'}>Sign UP</Link></p>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- Milestone-09/Module-50/login page/src/Pages/SignUp/SignUp.jsx
+++ Milestone-09/Module-50/login page/src/Pages/SignUp/SignUp.jsx
@@ -1,34 +1,68 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, sendEmailVerification, updateProfile } from "firebase/auth";
 import { useState } from "react"
 import { auth } from "../../authentication/firebase.init";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
 
-  const [users , setUsers] = useState(false);
-  const [errorMessage , setErrorMessage] = useState('')
+  const [users, setUsers] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('')
+  const [showPassword, setShowPassword] = useState(false)
 
-  const handleSignUp = (event) =>{
+  const handleSignUp = (event) => {
     event.preventDefault()
+
+    const name = event.target.name.value
+    const photoUrl = event.target.url.value
     const email = event.target.email.value
     const password = event.target.password.value
-    
+    const terms = event.target.terms.checked
+
+    console.log(name , photoUrl);
     // error message clear
     setErrorMessage('')
     setUsers(false)
-
-    if(password.length < 6){
+    if(!terms){
+      setErrorMessage('Please accept out terms and condition')
+      return;
+    }
+    if (password.length < 6) {
       setErrorMessage('Password must be 6 character')
       return;
-    }else{
-      createUserWithEmailAndPassword(auth , email , password)
+    }
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+    if (!passwordRegex.test(password)) {
+      setErrorMessage('at list 1 uppercase 1 lowercase 1 number 1 special character')
+      return;
+    }
+
+    // create a users section
+    createUserWithEmailAndPassword(auth, email, password)
       .then(res => {
         console.log(res);
         setUsers(true)
+
+        // send verification
+        sendEmailVerification(auth.currentUser)
+        .then(()=>{
+          alert('send verification');
+        })
+        // update users profile
+        const profile = {
+          displayName : name,
+          photoURL : photoUrl
+        }
+        updateProfile(auth.currentUser , profile)
+        .then(() =>{
+          console.log('users profile update');
+        }).catch(error =>{
+          setErrorMessage('users profile update error' , error.message)
+        })
       })
-      .catch(error =>{
+      .catch(error => {
         setErrorMessage(error.message);
       })
-    }
   }
 
   return (
@@ -39,24 +73,46 @@ const SignUp = () => {
 
           <div className="form-control">
             <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input type="text" name="name" placeholder="Name" className="input input-bordered" required />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Photo URL</span>
+            </label>
+            <input type="text" name="url" placeholder="Photo Url" className="input input-bordered" required />
+          </div>
+          <div className="form-control">
+            <label className="label">
               <span className="label-text">Email</span>
             </label>
             <input type="email" name="email" placeholder="email" className="input input-bordered" required />
           </div>
-          <div className="form-control">
+          <div className="form-control relative">
             <label className="label">
               <span className="label-text">Password</span>
             </label>
-            <input type="password" name="password" placeholder="password" className="input input-bordered"/>
+            <input type={showPassword ? 'text' : 'password'} name="password" placeholder="password" className="input input-bordered" />
+            <button onClick={() => setShowPassword(!showPassword)} className="absolute right-5 top-14 cursor-pointer">
+              {showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
+            </button>
             <label className="label">
               <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
             </label>
           </div>
+          <div className="form-control">
+            <label className="label cursor-pointer justify-start gap-5">
+              <input type="checkbox" name="terms" className="checkbox" />
+              <span className="label-text">Remember me</span>
+            </label>
+          </div>
           <div className="form-control mt-6">
-            <button className="btn btn-primary">Login</button>
-            {errorMessage&& <p className="text-center text-red-500">{errorMessage}</p>}
+            <button className="btn btn-primary">Sign Up</button>
+            {errorMessage && <p className="text-center text-red-500">{errorMessage}</p>}
           </div>
         </form>
+        <p className="text-center pb-2">Already Have a Account to <Link className="btn ml-2" to={'/login'}>Log in</Link></p>
       </div>
     </div>
   )
